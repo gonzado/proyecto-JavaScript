e55e8205c1366063f37730462d6add3a66bd12d6
gdocal <gondocal@gmail.com>
2022-10-11 19:40:47 +0200

    V2.0

--- script.js
+++ script.js
@@ -1,89 +1,69 @@
-function operaciones(op)
-{
+const productos = [
+    { nombre: "Camiseta", precio: 20 },
+    { nombre: "Pantalón", precio: 40 },
+    { nombre: "Jersey", precio: 45 },
+    { nombre: "Abrigo", precio: 70 },
+    { nombre: "Zapatos", precio: 65 },
+];
+let carrito = []
 
-    var ops = {
-        sumar: function sumarNumeros(n1, n2) {
-            return (parseInt(n1) + parseInt(n2));
-        },
+let seleccion = prompt("Encantado de saludarte, ¿Quieres comprar algún producto?. Si/No")
 
-        restar: function restarNumeros(n1, n2) {
-            return (parseInt(n1) - parseInt(n2));
-        },
-        
-        multiplicar: function multiplicarNumeros(n1, n2) {
-            return (parseInt(n1) * parseInt(n2));
-        },
+while(seleccion != "Si" && seleccion != "No"){
+    alert("Por favor ingresa Si/No")
+    seleccion = prompt("¿Quieres comprar algún producto?. Si/No")
+}
 
-        dividir: function dividirNumeros(n1, n2) {
-            return (parseInt(n1) / parseInt(n2));
+if(seleccion == "Si"){
+    alert("Estos son nuestros productos")
+    let todosLosProductos = productos.map(
+        (producto) => producto.nombre + " " + producto.precio + "$"
+    );
+    alert(todosLosProductos.join(" - "))
+} else if (seleccion == "No"){
+    alert("Gracias por su visita, hasta pronto.")
+}
+
+while(seleccion != "No"){
+    let producto = prompt("Añade un producto a tu cesta")
+    let precio = 0
+
+    if(producto == "Camiseta" || producto == "Pantalón" || producto == "Jersey" || producto == "Abrigo" || producto == "Zapatos"){
+        switch (producto){
+            case "Camiseta":
+                precio = 20;
+                break;
+            case "Pantalón":
+                precio = 40;
+                break;
+            case "Jersey":
+                precio = 45;
+                break;
+            case "Abrigo":
+                precio = 70;
+                break;
+            case "Zapatos":
+                precio = 65;
+                break;
         }
-
-
-    };
-
-    var operacion;
-
-    switch(op) {
-        case 'sumar':
-            var operando1 = document.getElementById("resultado").value;
-            document.getElementById("resultado").value = operando1 + "+";
-            operacion = document.getElementById("resultado").value;
-            document.getElementById("memoria").value = "suma";
-            break;
-        case 'restar':
-            var operando1 = document.getElementById("resultado").value;
-            document.getElementById("resultado").value = operando1 + "-";
-            operacion = document.getElementById("resultado").value;
-            document.getElementById("memoria").value = "resta";
-            break;
-        case 'multiplicar':
-            var operando1 = document.getElementById("resultado").value;
-            document.getElementById("resultado").value = operando1 + "*";
-            operacion = document.getElementById("resultado").value;
-            document.getElementById("memoria").value = "multiplicacion";
-            break;
-        case 'dividir':
-            var operando1 = document.getElementById("resultado").value;
-            document.getElementById("resultado").value = operando1 + "/";
-            operacion = document.getElementById("resultado").value;
-            document.getElementById("memoria").value = "division";
-            break;
-            
-        case 'igual':	
-            
-            operacion = document.getElementById("resultado").value;
-            
-            var memoriaop = document.getElementById("memoria").value;
-            
-            switch(memoriaop) {
-                case 'suma':
-                    var operandos = operacion.split("+");
-                    var resultado = ops.sumar(operandos[0], operandos[1]);
-                    document.getElementById("resultado").value = resultado;
-                    break;
-                case 'resta':
-                    var operandos = operacion.split("-");
-                    var resultado = ops.restar(operandos[0], operandos[1]);
-                    document.getElementById("resultado").value = resultado;
-                    break;
-                case 'multiplicacion':
-                    var operandos = operacion.split("*");
-                    var resultado = ops.multiplicar(operandos[0], operandos[1]);
-                    document.getElementById("resultado").value = resultado;
-                    break;
-                case 'division':
-                    var operandos = operacion.split("/");
-                    var resultado = ops.dividir(operandos[0], operandos[1]);
-                    document.getElementById("resultado").value = resultado;
-                    break;					
-            
-            }	
-            
-
-            break;
-            
-
+    let unidades = parseInt(prompt("Indique una cantidad"))
+    
+    carrito.push({producto, unidades, precio})
+    console.log(carrito)
+    } else{
+        alert("Producto erróneo")
     }
 
+    seleccion = prompt("¿Desea seguir comprando?")
 
+    while(seleccion === "No"){
+        alert("Gracias por su compra, vuelva pronto.")
+        carrito.forEach((carritoFinal) => {
+            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, Total a pagar por su compra ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+        break;
+    }
 }
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(`El total a pagar es: ${total}`);
